c1c41bade34478cc5dd8821b369b830ff4f5137f
DELL <DELL@DESKTOP-3QCIORS>
2024-08-29 13:03:26 +0530

    add code

--- bddframwork/src/test/java/locators/ResisterPageLocators.java
+++
@@ -1,9 +0,0 @@
-package locators;
-
-import org.openqa.selenium.By;
-
-public class ResisterPageLocators {
-  private By userName=By.id("username");
-  private By passWord=By.id("password");
-	     
-}
